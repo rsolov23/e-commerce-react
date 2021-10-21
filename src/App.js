c8c891b4828dc337f77e298d19af9e491735baf7
Rachel Solov <rsolov23@yahoo.com>
2021-10-21 17:10:22 -0500

    completed productList component and styling for Product component

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
-class App extends Component() {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={ProductList} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Default} />
-        </Switch>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
+  );
 }
+
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-
-
 export const storeProducts = [
   {
     id: 1,
