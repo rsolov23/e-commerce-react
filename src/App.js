00e460b55b98a90465ecafbb17107767da464d22
Rachel Solov <rsolov23@yahoo.com>
2021-10-29 11:04:55 -0500

    implemented modal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 function App() {
   return (
     <React.Fragment>
@@ -17,6 +17,7 @@ function App() {
         <Route path="/cart" component={Cart} />
         <Route component={Default} />
       </Switch>
+      <Modal />
     </React.Fragment>
   );
 }
--- src/components/Details.js
+++ src/components/Details.js
@@ -51,6 +51,7 @@ export default class Details extends Component {
                       disabled={inCart ? true : false}
                       onClick={() => {
                         value.addToCart(id);
+                        value.openModal(id);
                       }}
                     >
                       {inCart ? "inCart" : "add to cart"}
